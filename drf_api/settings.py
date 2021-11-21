3d6c83c8c39a9e754a4065507cff1c381dc92886
duajaradat <duajaradat164@gmail.com>
2021-11-21 20:55:50 +0200

    Api

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local app 
+    'library',
+    # third-party
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
